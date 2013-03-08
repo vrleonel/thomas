26f857398f19f88ef093112c9e6629ed9c400ad2
Vitor Leonel <vrleonel@gmail.com>
2013-03-07 23:13:50 -0300

    Pagina de information e inicio da página de galeria

--- wp-content/themes/thomas/page-information.php
+++ wp-content/themes/thomas/page-information.php
@@ -32,62 +32,63 @@ get_header(); ?>
 	</section>
 	
 	<section class="information">
+		<div>
+			<article>
+				<h4 class="vv">Tattoo appointment information</h4>
+				<p>
+				 I am currently not booking new clients for tattoo work. I am very grateful for the 
+				 people who want to get tattooed by myself and are willing to wait, without YOU / the client,
+				 my work would be nothing more than a dream. There are only so many hours in each day and I am 
+				 unfortunately unable to accommodate each inquiry and request. If you would like to get 
+				 tattooed by me in the future then please call Saved and they will give you any relevant information.
+				</p>
+				<p>
+					I try my best to answer all emails, but it is difficult sometimes to get back to everyone. 
+					If I don’t return your email please don’t hesitate to resend, they got lost quite easily, my 
+					current response time for emails is about 4-6 weeks as I handle all of this personally and it 
+					takes a long time to work my way through them between drawing and tattooing.
+				</p>
+			</article>
+			
 		
-		<article>
-			<h3 class="vv">Tattoo appointment information</h3>
-			<p>
-			 I am currently not booking new clients for tattoo work. I am very grateful for the 
-			 people who want to get tattooed by myself and are willing to wait, without YOU / the client,
-			 my work would be nothing more than a dream. There are only so many hours in each day and I am 
-			 unfortunately unable to accommodate each inquiry and request. If you would like to get 
-			 tattooed by me in the future then please call Saved and they will give you any relevant information.
-			</p>
-			<p>
+			
+			<article>
+				<h4 class="vv">Biography</h4>
+				<p>
+				I am currently not booking new clients for tattoo work. I am very grateful for 
+				the people who want to get tattooed by myself and are willing to wait, without 
+				YOU / the client, my work would be nothing more than a dream. There are only so 
+				many hours in each day and I am unfortunately unable to accommodate each inquiry and request. 
+				If you would like to get tattooed by me in the future then please call Saved and they will 
+				give you any relevant information.
+				</p>
+				<p>
 				I try my best to answer all emails, but it is difficult sometimes to get back to everyone. 
-				If I don’t return your email please don’t hesitate to resend, they got lost quite easily, my 
-				current response time for emails is about 4-6 weeks as I handle all of this personally and it 
-				takes a long time to work my way through them between drawing and tattooing.
-			</p>
-		</article>
-		
-	
-		
-		<article>
-			<h4 class="vv">Biography</h4>
-			<p>
-			I am currently not booking new clients for tattoo work. I am very grateful for 
-			the people who want to get tattooed by myself and are willing to wait, without 
-			YOU / the client, my work would be nothing more than a dream. There are only so 
-			many hours in each day and I am unfortunately unable to accommodate each inquiry and request. 
-			If you would like to get tattooed by me in the future then please call Saved and they will 
-			give you any relevant information.
-			</p>
-			<p>
-			I try my best to answer all emails, but it is difficult sometimes to get back to everyone. 
-			If I don’t return your email please don’t hesitate to resend, they got lost quite easily, 
-			my current response time for emails is about 4-6 weeks as I handle all of this personally 
-			and it takes a long time to work my way through them between drawing and tattooing.
-			</p>
-		</article>
+				If I don’t return your email please don’t hesitate to resend, they got lost quite easily, 
+				my current response time for emails is about 4-6 weeks as I handle all of this personally 
+				and it takes a long time to work my way through them between drawing and tattooing.
+				</p>
+			</article>
+		</div>
 
 		<aside>
 			<h4 class="vv">more thomas</h4>
-			<article>
+			<article class="item-1">
 				<span class="ico"></span>
-				<h4>Residual Self-Image</h4>
+				<h5>Residual Self-Image</h5>
 				<p>Personal exploration and some more describing what this is all about goes here.</p>
 					<a href="#">residualselfimage.wordpress.com</a>
 			</article>
-			<article>
+			<article class="item-2">
 				<span class="ico"></span>
-				<h4>Shirt And Destroy</h4>
+				<h5>Shirt And Destroy</h5>
 				<p>I’ve been creating and selling t-shirts with some friends and little more description of this.</p>
 				<a href="#">shirtsanddestroy.com/thomashooper</a>
 			</article>
 			
-			<article>
+			<article class="item-3">
 				<span class="ico"></span>
-				<h4></h4>
+				<h5>Artifact Publishing</h5>
 				<p>Limited series books and prints little description of this venture here.</p>
 				<a href="#">artifactpublishing.com</a>
 			</article>
