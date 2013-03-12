38ce519f0eeee493341e4185f498072b49c0795d
Vitor Leonel <vrleonel@gmail.com>
2013-03-12 00:37:23 -0300

    Adicionado imagens da pagina shop

---
+++ wp-content/themes/thomas/page-shop.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Template Name: Shop
+*/
+
+get_header(); ?>
+
+<div id="content" class="page-shop" role="main">
+  
+  <section class="clearfix">
+    
+    <aside>
+      <img src="<? bloginfo('template_directory');?>/img/img-1.jpg" width="405" height="405">
+    </aside>
+    
+    <article>
+      <h4>The Forgiven Ghost</h4>
+ 
+      <p>screenprinted this artwork as a 24 x 18" 6-color art print and published it 
+      in two different editions.</p>
+
+      <p>STANDARD & VARIANT EDITIONS <br>
+      100 prints signed & numbered by Thomas Hooper <br>
+      6 color print on Cougar white paper</p>
+
+      <p>buy online <br>
+      <a href="http://burlesquedesign.com/node/1790">http://burlesquedesign.com/node/1790</a></p>
+    </article>
+  </section>
+
+  <section class="clearfix">
+    
+    <aside>
+      <img src="<? bloginfo('template_directory');?>/img/img-2.jpg" width="405" height="405">
+    </aside>
+    
+    <article>
+      <h4>The Forgiven Ghost</h4>
+ 
+      <p>screenprinted this artwork as a 24 x 18" 6-color art print and published it 
+      in two different editions.</p>
+
+      <h5>STANDARD & VARIANT EDITIONS</h5>
+      <p>100 prints signed & numbered by Thomas Hooper <br>
+      6 color print on Cougar white paper</p>
+
+      <p>buy online <br>
+      <a href="http://burlesquedesign.com/node/1790">http://burlesquedesign.com/node/1790</a></p>
+    </article>
+  </section>
+
+  <section class="clearfix">
+    
+    <aside>
+      <img src="<? bloginfo('template_directory');?>/img/img-3.jpg" width="405" height="405">
+    </aside>
+    
+    <article>
+      <h4>The Forgiven Ghost</h4>
+ 
+      <p>screenprinted this artwork as a 24 x 18" 6-color art print and published it 
+      in two different editions.</p>
+
+      <h5>STANDARD & VARIANT EDITIONS</h5>
+      <p>100 prints signed & numbered by Thomas Hooper <br>
+      6 color print on Cougar white paper</p>
+
+      <p>buy online <br>
+      <a href="http://burlesquedesign.com/node/1790">http://burlesquedesign.com/node/1790</a></p>
+    </article>
+  </section>
+
+
+</div><!-- #content -->
+
+<?php get_footer(); ?>
