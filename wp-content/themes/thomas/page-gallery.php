1bee5d9869c42f4a864c5d895344eba5416e9cc7
Vitor Leonel <vrleonel@gmail.com>
2013-03-18 23:02:42 -0300

    trazendo links para a galeria de tatoo, falta a trazer os thumbs

--- wp-content/themes/thomas/page-gallery.php
+++ wp-content/themes/thomas/page-gallery.php
@@ -7,6 +7,25 @@ get_header(); ?>
 
 <div id="content" class="page-gallery" role="main">
 
+
+<?php
+global $post;
+$args = array(  'category' => 2, 'orderby' => 'post_date','order' => 'ASC', );
+$myposts = get_posts( $args );
+foreach( $myposts as $post ) :	setup_postdata($post); ?>
+
+	<div class="hexagon hexagon1">
+	    <div class="hexagon-in1">
+	    	<a href="<?php the_permalink(); ?>">
+	        	<div class="hexagon-in2" style="background-image: url(http://lorempixel.com/230/230);">
+	        		<span class="hex-hover"></span>
+	        	</div>
+	    	</a>
+	    </div>
+	</div>
+<?php endforeach; ?>
+
+
 	<div class="hexagon hexagon1">
 	    <div class="hexagon-in1">
 	    	<a href="">
