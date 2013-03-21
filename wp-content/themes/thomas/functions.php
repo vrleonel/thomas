16b4bdd03ea0136280130a8ab4a2861b6063d42f
Vitor Leonel <vrleonel@gmail.com>
2013-03-20 23:36:21 -0300

    Correcoes das galerias

--- wp-content/themes/thomas/functions.php
+++ wp-content/themes/thomas/functions.php
@@ -6,8 +6,6 @@ add_filter('previous_post_link_attributes', 'posts_link_attributes_2');
 
 add_theme_support( 'post-thumbnails' );
 
-add_image_size( 'category-thumb', 300, 9999 ); //300 pixels wide (and unlimited height)
-
 function posts_link_attributes_1() {
     return 'class="prev-post"';
 }
