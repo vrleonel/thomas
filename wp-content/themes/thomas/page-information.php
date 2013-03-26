a562871494126d4db832b612ee41713e2db7d2af
Vitor Leonel <vrleonel@gmail.com>
2013-03-25 23:36:41 -0300

    Construção de toda a área dinâmica de shop e information. Espaçamentos, sublinhados e negritos corrigidos

--- wp-content/themes/thomas/page-information.php
+++ wp-content/themes/thomas/page-information.php
@@ -10,27 +10,12 @@ get_header(); ?>
 	<section class="description clearfix">
 		
 		<aside>
-			<img src="<? bloginfo('template_directory');?>/img/thomas.jpg" width="405" height="405">
+			<img src="<?= get_field('description_image')?>" width="405" height="405">
 		</aside>
 		
 		<article>
 			<?= get_field("description_text"); ?>
-
-			<!-- <h4>My name is Thomas Hooper.</h4>
-			<p>Thank you so much for visiting my website and looking at my work. 
-			I was born in Hastings East Sussex, studied tattooing at Good Karma under Jim Macairt, 
-			studied Drawing at The London Institute of Art & Design, subsequently moving from London 
-			to New York City to pursue my goals in Tattooing and Art.</p>
-
-			<p>I work out of Saved Tattoo in Brooklyn NY. <br>
-			Please contact me there at</p> -->
-
-			<p class="adress"> 
-				<?= get_field("adress"); ?>
-			<!-- <strong>SAVED TATTOO</strong><br>
-			426 Union Avenue, Brooklyn, NY — (718) 486-0850 <br>
-			<a href="#">savedtattoo.com</a>  -->
-		</p>
+			<p class="adress"><?= get_field("adress"); ?></p>
 		</article>
 	</section>
 	
@@ -38,70 +23,16 @@ get_header(); ?>
 		<div>
 			<article>
 				<?= get_field("article_1")?>
-				<!-- <h4 class="vv">Tattoo appointment information</h4>
-				<p>
-				 I am currently not booking new clients for tattoo work. I am very grateful for the 
-				 people who want to get tattooed by myself and are willing to wait, without YOU / the client,
-				 my work would be nothing more than a dream. There are only so many hours in each day and I am 
-				 unfortunately unable to accommodate each inquiry and request. If you would like to get 
-				 tattooed by me in the future then please call Saved and they will give you any relevant information.
-				</p>
-				<p>
-					I try my best to answer all emails, but it is difficult sometimes to get back to everyone. 
-					If I don’t return your email please don’t hesitate to resend, they got lost quite easily, my 
-					current response time for emails is about 4-6 weeks as I handle all of this personally and it 
-					takes a long time to work my way through them between drawing and tattooing.
-				</p> -->
 			</article>
-			
-		
 			
 			<article>
 				<?= get_field("article_2")?>
-				<!-- <h4 class="vv">Biography</h4>
-				<p>
-				I am currently not booking new clients for tattoo work. I am very grateful for 
-				the people who want to get tattooed by myself and are willing to wait, without 
-				YOU / the client, my work would be nothing more than a dream. There are only so 
-				many hours in each day and I am unfortunately unable to accommodate each inquiry and request. 
-				If you would like to get tattooed by me in the future then please call Saved and they will 
-				give you any relevant information.
-				</p>
-				<p>
-				I try my best to answer all emails, but it is difficult sometimes to get back to everyone. 
-				If I don’t return your email please don’t hesitate to resend, they got lost quite easily, 
-				my current response time for emails is about 4-6 weeks as I handle all of this personally 
-				and it takes a long time to work my way through them between drawing and tattooing.
-				</p> -->
 			</article>
 		</div>
 
 		<aside>
 			<h4 class="vv">more thomas</h4>
-
 			<?= get_field("aside_articles") ?>
-			<!-- <article class="item-1">
-				<span class="ico"></span>
-				<h5>Residual Self-Image</h5>
-				<p>Personal exploration and some more describing what this is all about goes here.<br>
-					<a href="#">residualselfimage.wordpress.com</a>
-				</p>
-			</article>
-			<article class="item-2">
-				<span class="ico"></span>
-				<h5>Shirt And Destroy</h5>
-				<p>I’ve been creating and selling t-shirts with some friends and little more description of this.<br>
-					<a href="#">shirtsanddestroy.com/thomashooper</a>
-				</p>
-			</article>
-			
-			<article class="item-3">
-				<span class="ico"></span>
-				<h5>Artifact Publishing</h5>
-				<p>Limited series books and prints little description of this venture here.<br>
-					<a href="#">artifactpublishing.com</a>
-				</p>
-			</article> -->
 		</aside>
 	</section>
 
--- wp-content/themes/thomas/page-shop.php
+++ wp-content/themes/thomas/page-shop.php
@@ -10,63 +10,33 @@ get_header(); ?>
   <section class="clearfix">
     
     <aside>
-      <img src="<? bloginfo('template_directory');?>/img/img-1.jpg" width="405" height="405">
+      <img src="<?= get_field('aside_1') ?>" width="405" height="405">
     </aside>
     
     <article>
-      <h4>The Forgiven Ghost</h4>
- 
-      <p>screenprinted this artwork as a 24 x 18" 6-color art print and published it 
-      in two different editions.</p>
-
-      <p>STANDARD & VARIANT EDITIONS <br>
-      100 prints signed & numbered by Thomas Hooper <br>
-      6 color print on Cougar white paper</p>
-
-      <p class="small"><strong>BUY ONLINE</strong> <br>
-      <a href="http://burlesquedesign.com/node/1790">http://burlesquedesign.com/node/1790</a></p>
+      <?= get_field('article_1') ?>
     </article>
   </section>
 
   <section class="clearfix">
     
     <aside>
-      <img src="<? bloginfo('template_directory');?>/img/img-2.jpg" width="405" height="405">
+      <img src="<?= get_field('aside_2') ?>" width="405" height="405">
     </aside>
     
     <article>
-      <h4>The Forgiven Ghost</h4>
- 
-      <p>screenprinted this artwork as a 24 x 18" 6-color art print and published it 
-      in two different editions.</p>
-
-      <p>STANDARD & VARIANT EDITIONS <br>
-      100 prints signed & numbered by Thomas Hooper <br>
-      6 color print on Cougar white paper</p>
-
-      <p class="small"><strong>BUY ONLINE</strong><br>
-      <a href="http://burlesquedesign.com/node/1790">http://burlesquedesign.com/node/1790</a></p>
+      <?= get_field('article_2') ?>
     </article>
   </section>
 
   <section class="clearfix">
     
     <aside>
-      <img src="<? bloginfo('template_directory');?>/img/img-3.jpg" width="405" height="405">
+      <img src="<?= get_field('aside_3') ?>" width="405" height="405">
     </aside>
     
     <article>
-      <h4>The Forgiven Ghost</h4>
- 
-      <p>screenprinted this artwork as a 24 x 18" 6-color art print and published it 
-      in two different editions.</p>
-
-      <p>STANDARD & VARIANT EDITIONS <br>
-      100 prints signed & numbered by Thomas Hooper <br>
-      6 color print on Cougar white paper</p>
-
-      <p class="small"><strong>BUY ONLINE</strong><br>
-      <a href="http://burlesquedesign.com/node/1790">http://burlesquedesign.com/node/1790</a></p>
+      <?= get_field('article_3') ?>
     </article>
   </section>
 
