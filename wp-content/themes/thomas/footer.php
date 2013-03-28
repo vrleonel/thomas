f3f24a17aa2c5c45eafaef4e6db38c5acd53c708
Vitor Leonel <vrleonel@gmail.com>
2013-03-28 03:55:58 -0300

    spritely adicionado como biblioteca

--- wp-content/themes/thomas/footer.php
+++ wp-content/themes/thomas/footer.php
@@ -1,57 +1,42 @@
 <?php
 /* Menu */
 $main_menu_args = array(
-	'theme_location'  => 'main-menu',
-	'container'       => 'div',
-	'menu_id'         => 'main-menu',
-	'menu_class'      => 'left',
-	'echo'            => true,
-	'fallback_cb'     => 'wp_page_menu',
-	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-	'depth'           => 0,
-	'walker'          => ''
+  'theme_location'  => 'main-menu',
+  'container'       => 'div',
+  'menu_id'         => 'main-menu',
+  'menu_class'      => 'left',
+  'echo'            => true,
+  'fallback_cb'     => 'wp_page_menu',
+  'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+  'depth'           => 0,
+  'walker'          => ''
 );
 
 $social_menu_args = array(
-	'theme_location'  => 'social-menu',
-	'container'       => 'div',
-	'menu_id'         => 'social-menu',
-	'menu_class'      => 'right social-links',
-	'echo'            => true,
-	'fallback_cb'     => 'wp_page_menu',
-	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-	'depth'           => 0,
-	'walker'          => ''
+  'theme_location'  => 'social-menu',
+  'container'       => 'div',
+  'menu_id'         => 'social-menu',
+  'menu_class'      => 'right social-links',
+  'echo'            => true,
+  'fallback_cb'     => 'wp_page_menu',
+  'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+  'depth'           => 0,
+  'walker'          => ''
 );
 
 ?>
-	</div><!-- #main -->
-	<div id="footer">
-		<nav id="navigator">	
-			<!--ul class="left">
-				<li><a href="<?= home_url('/tatoo');?>">tatoo art</a> </li>
-				<li><a href="#">painting</a></li>
-				<li><a href="<?= home_url('/information');?>">information</a></li>
-				<li><a href="#">blog/news</a></li>
-				<li><a href="<?= home_url('/shop');?>">shop</a></li>
-			</ul-->
-
-			<? wp_nav_menu( $main_menu_args ); ?>
-			<? wp_nav_menu( $social_menu_args ); ?>
-
-			<!-- <ul class="right social-links">
-				<li><a href="#">e-mail</a></li>
-				<li><a href="#">instagram</a> </li>
-				<li><a href="#">facebook</a></li>
-				<li><a href="#">tumblr</a></li>
-			</ul> -->
-		</nav>
-	</div>
+  </div><!-- #main -->
+  <div id="footer">
+    <nav id="navigator">
+      <? wp_nav_menu( $main_menu_args ); ?>
+      <? wp_nav_menu( $social_menu_args ); ?>
+    </nav>
+  </div>
 </div><!-- #wrapper -->
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.simplemodal-1.4.4.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.spritely.js"></script>
 <script src="<? bloginfo('template_directory');?>/js/plugins.js"></script>
 <script src="<? bloginfo('template_directory');?>/js/main.js"></script>
 
@@ -64,12 +49,12 @@ $social_menu_args = array(
 
 
 <?php
-	/* Always have wp_footer() just before the closing </body>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to reference JavaScript files.
-	 */
+  /* Always have wp_footer() just before the closing </body>
+   * tag of your theme, or you will break many plugins, which
+   * generally use this hook to reference JavaScript files.
+   */
 
-	wp_footer();
+  wp_footer();
 ?>
 </body>
 </html>
